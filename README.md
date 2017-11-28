41b30f7964bfb0ec9dfc0819660528f61efaa87c
Hayk Ghazaryan <hayk.ghazaryan@veeva.com>
2017-11-28 11:32:17 -0500

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
 # babel-plugin-transform-html-to-primitives
+
 Transforms HTML tags in JSX to their Primitive equivalents
+
+## Example
+
+**In**
+
+```js
+import React from 'react';
+import { Image, View, Text } from 'react-primitives';
+import styled from 'styled-components/primitives';
+
+
+const ButtonElement = styled.button`
+    height: 50px;
+    padding: 0 10px;
+    border-radius: 3px;
+	background-color: #595959;
+`;
+
+const textStyle = {
+    lineHeight: 50,
+    color: '#FFFFFF',
+    textAlign: 'center'
+};
+
+
+const Button = ({ relative }) => (
+  <ButtonElement>
+  	<img src="/photo.jpg" alt={relative} />
+  	<span style={textStyle}>Luke, I am your <strong className="relative">{relative}</strong>.</span>
+  </ButtonElement>
+);
+
+Button.defaultProps = {
+    relative: 'father'
+};
+
+export default Button;
+```
+
+**Out**
+
+```js
+import React from 'react';
+import { Image, View, Text } from 'react-primitives';
+import styled from 'styled-components/primitives';
+
+
+const ButtonElement = styled(View)`
+    height: 50px;
+    padding: 0 10px;
+    border-radius: 3px;
+	background-color: #595959;
+`;
+
+const textStyle = {
+    lineHeight: 50,
+    color: '#FFFFFF',
+    textAlign: 'center'
+};
+
+
+const Button = ({ relative }) => (
+  <ButtonElement>
+  	<Image source="/photo.jpg" alt={relative} />
+  	<Text style={textStyle}>Luke, I am your <Text className="relative">{relative}</Text>.</Text>
+  </ButtonElement>
+);
+
+Button.defaultProps = {
+    relative: 'father'
+};
+
+export default Button;
+```
+
+## Installation
+
+```sh
+$ npm install babel-plugin-transform-html-to-primitives
+```
+
+## Usage
+
+### Via `.babelrc` (Recommended)
+
+**.babelrc**
+
+```json
+{
+  "plugins": ["transform-html-to-primitives"]
+}
+```
+
+### Via CLI
+
+```sh
+$ babel --plugins transform-html-to-primitives script.js
+```
+
+### Via Node API
+
+```javascript
+require("babel-core").transform("code", {
+  plugins: ["transform-html-to-primitives"]
+});
+```
---
+++ index.js
@@ -0,0 +1,192 @@
+// import { types as t } from "@babel/core";
+
+const setAttribute = (t, attributes, name, value) => {
+  let isAttributeSet = false;
+
+  if (!attributes) {
+    attributes = [];
+  }
+
+  for (let i in attributes) {
+    if (!attributes.hasOwnProperty(i)) {
+      continue;
+    }
+
+    // Overriding the value if attribute exists
+    if (attributes[i].name.name === name) {
+      attributes[i] = t.JSXAttribute(t.JSXIdentifier(name),
+                                     t.StringLiteral(value)
+                                    );
+      isAttributeSet = true;
+    }
+  }
+
+  // add new attribute if existing attribute wasn't overriden
+  if (!isAttributeSet) {
+    attributes.unshift(
+      t.JSXAttribute(t.JSXIdentifier(name),
+                     t.StringLiteral(value)
+                    )
+    );
+  }
+};
+
+const setImageSourceAttribute = (t, attributes) => {
+    if (!attributes) {
+      attributes = [];
+    }
+
+    for (let i in attributes) {
+      if (!attributes.hasOwnProperty(i)) {
+        continue;
+      }
+
+      // Overriding the value if attribute exists
+      if (attributes[i].name.name === 'src') {
+        attributes[i] = t.JSXAttribute(t.JSXIdentifier('source'),
+                                       t.StringLiteral(attributes[i].value.value)
+                                      );
+      }
+    }
+};
+
+const TextTags = [
+    'a', 'abbr', 'address',
+    'b', 'bdi', 'bdo', 'big', 'blockquote', 'br',
+    'cite', 'code',
+    'data', 'del', 'dfn',
+    'em',
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'i', 'ins',
+    'kbd',
+    'label', 'legend',
+    'mark', 'marquee', 'meter',
+    'output',
+    'p', 'pre',
+    'q',
+    'rp', 'rt', 'ruby',
+    's', 'samp', 'small', 'span', 'strong', 'sub', 'summary', 'sup',
+    'time',
+    'u',
+    'var',
+    'wbr'
+];
+
+const ImageTags = [
+    'img'
+];
+
+const ViewTags = [
+    'area', 'article', 'aside', 'audio',
+    'base',  'body', 'button',
+    'canvas', 'caption', 'col', 'colgroup',
+    'datalist', 'dd', 'details', 'dialog', 'div', 'dl', 'dt',
+    'embed',
+    'fieldset', 'figcaption', 'figure', 'footer', 'form',
+    'head', 'header', 'hgroup', 'hr', 'html',
+    'iframe', 'input',
+    'keygen',
+    'li', 'link',
+    'main', 'map', 'menu', 'menuitem', 'meta',
+    'nav', 'noscript',
+    'object', 'ol', 'optgroup', 'option',
+    'param', 'picture', 'progress',
+    'script', 'section', 'select', 'source', 'style',
+    'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'track',
+    'ul',
+    'video'
+];
+
+const SVGTags = [
+    'circle', 'clipPath',
+    'defs',
+    'ellipse',
+    'g',
+    'image',
+    'line', 'linearGradient',
+    'mask',
+    'path', 'pattern', 'polygon', 'polyline',
+    'radialGradient', 'rect',
+    'stop', 'svg',
+    'text', 'tspan'
+];
+
+
+export default function ({types: t }) {
+  const DefaultPrimitives = {};
+
+  for (const tag of TextTags) {
+    DefaultPrimitives[tag] = 'Text';
+  }
+  for (const tag of ImageTags) {
+    DefaultPrimitives[tag] = 'Image';
+  }
+  for (const tag of ViewTags) {
+    DefaultPrimitives[tag] = 'View';
+  }
+
+  return {
+    visitor: {
+      TaggedTemplateExpression(path, state) {
+        const objectName = path.node.tag && path.node.tag.object && path.node.tag.object.name || false;
+        const tagName = path.node.tag && path.node.tag.property && path.node.tag.property.name || false;
+      	const Primitives = Object.assign(DefaultPrimitives, state.opts.primitives);
+        const isPrimitiveTag = tagName && Object.keys(Primitives).includes(tagName);
+        const isStyledComponents = objectName && objectName === 'styled';
+
+        if (isStyledComponents && isPrimitiveTag) {
+           path.node.tag = t.CallExpression(t.Identifier('styled'), [t.Identifier(Primitives[tagName])]);
+           // path.node.tag = t.MemberExpression(t.Identifier('styled'), t.Identifier(Primitives[tagName]));
+        }
+      },
+      JSXIdentifier(path, state) {
+        const tagNameProp = state.opts.tagNameProp;
+        const Primitives = Object.assign(DefaultPrimitives, state.opts.primitives);
+
+        let tagName = path.node.name,
+            isPrimitiveTag = Object.keys(Primitives).includes(tagName);
+
+        if (isPrimitiveTag) {
+        	path.node.name = Primitives[tagName];
+
+          	if (tagNameProp) {
+        		setAttribute(t, path.parent.attributes, tagNameProp, tagName);
+            }
+            if (path.node.name === 'Image') {
+                setImageSourceAttribute(t, path.parent.attributes);
+            }
+        }
+
+        // Ignore attributes
+        path.stop();
+
+      },
+      JSXText(path, state) {
+        return;
+        if(path.parent.openingElement.name.name !== 'Text') {
+        let text = path.node.value.replace(/^[\s]+/, '').replace(/[\s]+$/, '');
+        if (text.length > 0) {
+          let prefixWhitespace = path.node.value.replace(/[\s]+$/, '').replace(text, '');
+          let suffixWhitespace = path.node.value.replace(/^[\s]+/, '').replace(text, '');
+
+          path.replaceWithMultiple([
+            t.JSXText(prefixWhitespace),
+            t.JSXElement(
+              t.JSXOpeningElement(
+                t.JSXIdentifier('Text'),
+                []
+              ),
+              t.JSXClosingElement(
+                t.JSXIdentifier('Text')
+              ),
+              [t.JSXText(text)]
+            ),
+            t.JSXText(suffixWhitespace)
+          ]);
+        }
+        }
+
+      }
+    }
+  };
+}
